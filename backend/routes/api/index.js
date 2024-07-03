a582637264dc3d466340b99b2461bbd4faa2f4d4
root <root@Kishi-Kitsune>
2024-07-03 16:37:07 -0600

    added session router into api index, fixed imports

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const sessionRouter = require('./session.js');
+const sessionRouter = require('./session-router');
 const usersRouter = require('./user-router');
 const heroRouter = require('./hero-router');
 const { restoreUser } = require('../utils/auth');
@@ -10,7 +10,7 @@ const { restoreUser } = require('../utils/auth');
 // Else sets req.user to null
 router.use(restoreUser);
 
-// router.use('/session', sessionRouter);
+router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
@@ -18,6 +18,15 @@ router.use('/heroes', heroRouter)
 
 router.get("/", (_req, res, _next) => {
     res.json("Welcome to the Moba-Builder API!")
+});
+
+// reset CSRF token, set the cookie, return XSRF-TOKEN cookie
+router.get("/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
 });
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,15 +8,4 @@ router.get("/", (req, res, next) => {
     res.json("Welcome to the Moba-Builder Home!")
 });
 
-// reset CSRF token, set the cookie, return XSRF-TOKEN cookie
-router.get("/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    });
-});
-
-
-
 module.exports = router;
